a82111a6142a9f865afdacf56640c1c1b04459c3
MOhsan Ali <amohsan12345678@gmail.com>
2024-08-06 19:14:25 +0500

    added some style on logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,12 +57,11 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="/" class="brand-link">
-            <img src="{{ asset('images/OPUS S W.png') }}" alt="AdminLTE Logo"
-                 class="brand-image img-circle elevation-3"
-                 style="opacity: .8">
-            <span class="brand-text font-weight-light">ZIMO OPUS</span>
+        <a href="/" class="brand-link m-6">
+            <img src="{{ asset('images/OPUS S W.png') }}" alt="Brand Logo"  style="width: 50%; box-shadow: 0 8px 8px rgba(0, 0, 0, 0.3);" >
         </a>
+
+
 
         @include('layouts.navigation')
     </aside>
